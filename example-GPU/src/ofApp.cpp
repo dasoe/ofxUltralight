e35440e242303a7075f2c6b5cb3cdd193be2205a
das <oe>
2021-03-16 09:19:14 +0100

    non working still, but compile success. Add opencv
    Note. debug dlls for opencv (byun) are not included - too big for github

--- example-GPU/src/ofApp.cpp
+++ example-GPU/src/ofApp.cpp
@@ -1,71 +1,56 @@
 #include "ofApp.h"
 
-//--------------------------------------------------------------
-void ofApp::setup(){
+//
+// This example show basic integrating of a Browser in an OF App.
+// 
 
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	// setup Ultralight. Params: int width, int height, [ofVec2f offset], string url/file
+	// url or file is distinguished automatically by checking the beginning of the string (http(s))
+	// last parameter is a boolean to switch on GPU rendering
+
+	ul.setup(1400, 700, "https://de.wikipedia.org/wiki/Eisvogel", true);
+	// ul.setup( 600, 400, "file.html" );
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
-
+void ofApp::update() {
+	ul.update();
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
-
+void ofApp::draw() {
+	ul.draw();
+	ofDrawBitmapStringHighlight(ofToString(ofGetFrameRate()), 10, 10);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
-
+void ofApp::keyPressed(int key) {
+	ul.keyPressed(key);
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
+void ofApp::keyReleased(int key) {
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
+void ofApp::mouseMoved(int x, int y) {
+	ul.mouseMoved(x, y);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
+void ofApp::mousePressed(int x, int y, int button) {
+	ul.mousePressed(x, y, button);
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
+void ofApp::mouseReleased(int x, int y, int button) {
+	ul.mouseReleased(x, y, button);
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY) {
+	ul.mouseScrolled(x, y, scrollX, scrollY);
 }
